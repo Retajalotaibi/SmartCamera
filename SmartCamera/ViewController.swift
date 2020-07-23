ed8a31425d52e6fa391713731aee907ada10c052
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-07-24 02:34:39 +0300

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by Retaj Al-Otaibi on 7/24/20.
+//  Copyright © 2020 Retaj Al-Otaibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
