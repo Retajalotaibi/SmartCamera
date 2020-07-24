912cfc0f7b976778cfcde1752ff1762d4a74a0f1
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-07-24 03:58:54 +0300

    done with the demo

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import AVKit
+import Vision
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIView!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        //MARK: - CAMERA 📷
+        //her is where we start up the camera
+        
+        let capturSession = AVCaptureSession()
+        capturSession.sessionPreset = .photo
+        
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return} // needs to be unwrapped
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}// the device could have no camers (throws error)
+        
+        // here we add input so the captur start (could be audio input or in oue case camra)
+        capturSession.addInput(input)
+        
+        capturSession.startRunning()
+        // seting the camera output
+        let previewLayer = AVCaptureVideoPreviewLayer(session: capturSession)
+        imageView.layer.addSublayer(previewLayer)
+        previewLayer.frame = imageView.frame// setting frame
+        
+        // to check every frame
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        capturSession.addOutput(dataOutput)
+        
+        
+        
+    
     }
 
+         //MARK: - analyse what is this image 🤔
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
 
+              guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
+              let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+                 
+                  guard let results = finishedReq.results as? [VNClassificationObservation]else {return}
+                  guard let firstObservation = results.first else {return}
+                DispatchQueue.main.async {  self.label.text = "\(firstObservation.identifier)    \(firstObservation.confidence)"}
+                  print("\(firstObservation.identifier)    \(firstObservation.confidence)")
+              }
+             try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
 }
 
